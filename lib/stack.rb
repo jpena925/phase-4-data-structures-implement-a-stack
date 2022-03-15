7b25d2dbba301335caabc1e14f50dd55ffa88a36
Jack Pena <penajackc@gmail.com>
2022-03-15 15:33:08 -0700

    commit tree 4ea4a172cf51d25cf9cccf977f3162d541c15263

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,43 @@
 # your code here
+class Stack
+
+    attr_reader :limit
+
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(value)
+        raise 'Stack Overflow' if full?
+        @stack.push(value)
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+
+    def size
+        @stack.length
+    end
+
+    def empty?
+        @stack.length == 0
+    end
+
+    def full?
+        @stack.length == @limit
+    end
+
+    def search(value)
+
+        @stack.index(value) ? (size - 1) - @stack.index(value) : -1
+    end
+
+
+    
+end
